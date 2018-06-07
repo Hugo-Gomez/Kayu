913f0ef86a094b0266b5eac7d7e07efcba44d9d0
Hugo Gomez <hugo.gomez9795@gmail.com>
2018-06-07 16:39:56 +0200

    Dashboard

--- resources/views/layouts/structure.blade.php
+++ resources/views/layouts/structure.blade.php
@@ -48,7 +48,7 @@
                     <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
                         <div class="menu_section">
                         <ul class="nav side-menu">
-                            <li><a><i class="fa fa-tachometer-alt"></i> Dashboard </a>
+                        <li class="dashboard-menu"><a href="{{ route('dashboard') }}"><i class="fa fa-tachometer-alt"></i> Dashboard </a>
                             </li>
                             <li><a><i class="fa fa-sliders-h"></i> Préférences <span class="fa fa-chevron-down"></span></a>
                             <ul class="nav child_menu">
